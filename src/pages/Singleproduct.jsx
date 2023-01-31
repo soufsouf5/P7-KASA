c284d58246b6c3b9539763b3eca4695eb5c36714
soufsouf5 <oummad.soufiane@gmail.com>
2023-01-31 16:16:01 +0100

    add number slides and page error product id

--- src/pages/Singleproduct.jsx
+++ src/pages/Singleproduct.jsx
@@ -6,20 +6,23 @@ import Server from "../components/Server";
 import Slider from "../components/Slider";
 import Tags from "../components/Tags";
 import Collapse from "../components/Collapse";
+import PageNotFound from "../pages/PageNotFound";
 
 const SingleProduct = () => {
   const { productId } = useParams();
   const product = products.find((product) => product.id === productId);
-  const { title, location, rating, host, equipments, description, pictures } =
-    product;
 
   return (
-    <div className="singleproduct">
-      <Slider slides={pictures} />
+    <>
+      {!product?(
+      <PageNotFound />
+      ):(
+        <div className="singleproduct">
+      <Slider slides={product.pictures} />
       <div className="singleproduct__content">
         <div className="singleproduct__informations">
-          <h1 className="singleproduct__title">{title}</h1>
-          <p className="singleproduct__location">{location}</p>
+          <h1 className="singleproduct__title">{product.title}</h1>
+          <p className="singleproduct__location">{product.location}</p>
           <div className="singleproduct__tags">
             {product.tags.map((tag, index) => (
               <Tags key={index} getTag={tag} />
@@ -27,15 +30,18 @@ const SingleProduct = () => {
           </div>
         </div>
         <div className="singleproduct__rating-and-host">
-          <Rating rating={rating} />
-          <Server host={host} />
+          <Rating rating={product.rating} />
+          <Server host={product.host} />
         </div>
       </div>
       <div className="singleproduct__dropdowns">
-        <Collapse title="description" content={description} />
-        <Collapse title="équipements" content={equipments} />
+        <Collapse title="description" content={product.description} />
+        <Collapse title="équipements" content={product.equipments} />
       </div>
-    </div>
+      </div>
+      )
+      }
+    </>
   );
 };
 
